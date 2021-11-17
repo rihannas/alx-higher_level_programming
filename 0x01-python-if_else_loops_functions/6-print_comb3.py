dbbd16e69267fe29a0d0c23880c9a6bbbd64d16a
avocadoclouds <rehana447@gmail.com>
2021-11-16 17:40:26 -0800

    add'

---
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+for a in range(10):
+    for b in range(10):
+        if (a == 8) and (b == 9): 
+            print("{}{}".format(a, b))
+        elif (a != b) and (a < b):
+            print("{}{}".format(a, b), end=", ")
