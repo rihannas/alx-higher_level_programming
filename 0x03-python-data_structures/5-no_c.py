ed8eaa32e1fb528e65213096e68b3156be68f50e
avocadoclouds <rehana447@gmail.com>
2021-11-22 01:28:53 -0800

    add

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -2,8 +2,7 @@
 def no_c(my_string):
     c = [C, c]
     for i in my_string:
-        if i != c:
-            print(f{my_string})
-        else:
+        new_string = ""
+        if i == c:
             new_string = new_string + i
         return new_string
