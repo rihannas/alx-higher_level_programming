c940eed0e685426df49d62fabf3e15b9fb57f1c9
avocadoclouds <rehana447@gmail.com>
2021-11-22 01:26:16 -0800

    add

---
+++ 0x03-python-data_structures/5-no_c.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+def no_c(my_string):
+    c = [C, c]
+    for i in my_string:
+        if i != c:
+            print(f{my_string})
+        else:
+            new_string = new_string + i
+        return new_string
