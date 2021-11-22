129568d6d88c7dee9cd752eb28423be84558e4c4
avocadoclouds <rehana447@gmail.com>
2021-11-22 13:48:42 -0800

    add'

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/python3
-def print_matrix_integer(matrix=[[]]):
-    for row in (len(matrix)):
-        for column in (len(matrix[i])):
-            print("{:d}".format(matrix[i][j]), end="")
-            if column != (len(matrix[i]) - 1):
-                print(" ", end="")
-        print()
