1251118b35ea2a655b558ed848a8309c898bb85f
avocadoclouds <rehana447@gmail.com>
2021-11-22 14:03:37 -0800

    push

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -2,5 +2,7 @@
 def print_matrix_integer(matrix=[[]]):
     for i in matrix:
         for j in matrix:
-            print("{:d}".format(col), end=" " if i != j[-1] else "")
-        print()
+            print("{:d}".format(j), end="")
+            if j != i[-1]:
+                 print(" ", end="")
+        print("")
