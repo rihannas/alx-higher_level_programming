57c1b7d6a8d599e6912c64e98e330a40c74a738f
avocadoclouds <rehana447@gmail.com>
2021-11-22 01:10:08 -0800

    add

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -3,7 +3,7 @@ def new_in_list(my_list, idx, element):
     if idx > 0:
         return my_list
     elif idx > (len(my_list) - 1):
-        return m_list
+        return my_list
     else:
         new_list[idx] = element
         return new_list
