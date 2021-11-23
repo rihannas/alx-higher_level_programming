f13666b398e3c44f1d0070cd2cd64305ca0e72c1
avocadoclouds <rehana447@gmail.com>
2021-11-22 16:44:40 -0800

    add

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
     if sentence == "":
-        1stchar = "None"
+        1stchar = None
     else:
         1stchar = sequence[0]
         length = len(sequence)
