32e4c7354c81cd314c88ee45b0046a0e5093981f
avocadoclouds <rehana447@gmail.com>
2021-11-22 18:21:27 -0800

    c

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -2,6 +2,6 @@
 def no_c(my_string):
     new_string = ""
     for i in my_string:
-        if i != c and i != C:
-            new_string = new_string + i
+        if my_string[i] != c or my_string[i]!= C:
+            new_string = new_string + my_string[i]
         return new_string
