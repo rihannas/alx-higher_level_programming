eb1588b6c48594944a589cf635e6cf5784add41f
avocadoclouds <rehana447@gmail.com>
2021-11-22 16:20:36 -0800

    add

---
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def add_tuple(tuple_a=(), tuple_b=()):
+    la = len(tuple_a)
+    lb = len(tuple_b)
+
+    if la < 2 or lb < 2:
+        tuple_a = tuple_a + (0,)
+        tuple_b = tuple_b + (0,)
+        new = tuple_a[0] + tuple_b[0], tuple_a[1] + tuple_b[1]
+    print(new)
