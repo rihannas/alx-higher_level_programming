5dc881a71599853b7093c78b19bb25f05d476000
avocadoclouds <rehana447@gmail.com>
2021-11-22 16:46:07 -0800

    see

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -4,5 +4,5 @@ def multiple_returns(sentence):
         1stchar = None
     else:
         1stchar = sequence[0]
-        length = len(sequence)
+    length = len(sequence)
     return 1stchar, length
