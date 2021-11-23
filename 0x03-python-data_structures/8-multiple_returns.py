be0b6bfc330b1034dda0761cc8c6520594593bb3
avocadoclouds <rehana447@gmail.com>
2021-11-22 17:05:00 -0800

    add

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -4,5 +4,5 @@ def multiple_returns(sentence):
     if len(sentence) == 0:
         tup = None
     else:
-        tup = sentence[0], len(sentence)
+        tup = len(sentence), sentence[0]
     return tup
