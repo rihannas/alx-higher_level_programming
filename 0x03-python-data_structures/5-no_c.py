d56641181cc8b8c4ee4274601e5ba03c96fb65c5
avocadoclouds <rehana447@gmail.com>
2021-11-22 18:34:53 -0800

    add

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -3,6 +3,6 @@ def no_c(my_string):
     new_string = ""
     i = 0
     for i in my_string:
-        if i != char(99) or i != char(67):
+        if i != "C" and i != "c":
             new_string = new_string + i
     return new_string
