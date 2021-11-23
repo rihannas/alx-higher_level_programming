dde956ab827209bc98d52e0e255f3c3a39fa0f1d
avocadoclouds <rehana447@gmail.com>
2021-11-23 13:20:01 -0800

    add

---
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -0,0 +1,22 @@
+f __name__ == "__main__":
+    from calculator_1 import add, sub, mul, div
+    import argv from sys
+
+    if len(argv) != 4:
+        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
+        exit(1)
+    a = int(argv[1])
+    b = int(argv[3])
+    operator = argv[2]
+
+    if operator == '+':
+        print(f"{a} {operator} {b} = {a + b}")
+    elif operator == '-':
+        print(f"{a} {operator} {b} = {a - b}")
+    elif operator == '*':
+        print(f"{a} {operator} {b} = {a * b}")
+    elif operator == '/':
+        print(f"{a} {operator} {b} = {a * b}")
+    else:
+        print("Unknown operator. Available operators: +, -, * and /")
+        exit(1)
