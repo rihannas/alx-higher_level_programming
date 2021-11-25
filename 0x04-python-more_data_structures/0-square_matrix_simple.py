d59b92b47789b518eeb37c6348538b08f75974e6
avocadoclouds <rehana447@gmail.com>
2021-11-24 16:52:09 -0800

    add

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
-    sqrmatrix = [[x ** 2 for x in i] for in matrix]
+    sqrmatrix = [[x ** 2 for x in i] for i in matrix]
     return sqrmatrix
