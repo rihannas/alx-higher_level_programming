faa7bd776a9223d2882e17e3f4ae0ca267b50dfa
avocadoclouds <rehana447@gmail.com>
2021-11-24 18:29:32 -0800

    redo

--- 0x04-python-more_data_structures/7-update_dictionary.py
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 def update_dictionary(a_dictionary, key, value):
-    if key is in a_dictionary:
-        for i in a_dictionary:
-            i = key
-            a_dictionary[i] = value
-    else:
+    if key not in a_dictionary:
         a_dictionary[key] = value
+    else:
+        for i in a_dictionary:
+            if i == key:
+                a_dictionary[i] = value
     return a_dictionary
