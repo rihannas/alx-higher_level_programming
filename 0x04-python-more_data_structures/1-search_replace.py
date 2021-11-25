2a3592543c70803a83bceb1ca2a57adf15d7a2f9
avocadoclouds <rehana447@gmail.com>
2021-11-24 17:28:32 -0800

    a

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,12 +1,9 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
-    if my_list == None:
-        return None
-    else:
-        new = my_list.copy()
-        for i in range(new):
-            if i == search:
-                new.append(replace)
-            else: 
-                new.append(i)
+    new = my_list.copy()
+    for i in range(new):
+        if i == search:
+            new.append(replace)
+        else:
+            new.append(i)
     return new
