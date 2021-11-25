7707c0cc3f47c65c1829385584eeda16f1806f9c
avocadoclouds <rehana447@gmail.com>
2021-11-24 17:11:17 -0800

    a

---
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def search_replace(my_list, search, replace):
+    if my_list == None:
+        return None
+    else:
+        new = []
+        for i in my_list:
+            if i == search:
+                copy.append(replace)
+        return copy
