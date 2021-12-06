de9cfb58b70831778c84aa9df81d4cf5bef51e3e
avocadoclouds <rehana447@gmail.com>
2021-12-06 03:19:15 -0800

    add

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -3,8 +3,10 @@
 def safe_print_division(a, b):
     try:
         result = a / b
-    except (ZeroDivisionError, FloatingPointError):
+        return result
+    except (ZeroDivisionError):
         result = None
+        return result
     finally:
         print("Inside result: {}".format(result))
     return result
