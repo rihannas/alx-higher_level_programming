f84638247d4c642dbde9504ba26ede0a9c5c3c5a
avocadoclouds <rehana447@gmail.com>
2021-12-06 02:42:41 -0800

    add

---
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+def safe_print_list_integers(my_list=[], x=0):
+    start = 0
+    for i in range(0, x):
+        try:
+            print(my_list[i], end="")
+            start += 1
+        except (TypeError, ValueError):
+            break
+
+    print()
+
+    return start
