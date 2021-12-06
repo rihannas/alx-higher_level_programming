405e28911ae9b6c8f72e87ffdd684573fe5dfee2
avocadoclouds <rehana447@gmail.com>
2021-12-06 03:46:09 -0800

    add

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -2,7 +2,7 @@
 def list_division(my_list_1, my_list_2, list_length):
     newlist = []
     result = 0
-    for i in range(list_length):
+    for i in range(0, list_length):
         try:
             result = my_list_1 / my_list_2
         except ZeroDivisionError:
