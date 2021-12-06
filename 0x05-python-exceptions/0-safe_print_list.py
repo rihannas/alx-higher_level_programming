6a307269af0ba94883cc740bb620e4903f7608af
avocadoclouds <rehana447@gmail.com>
2021-12-06 01:48:10 -0800

    update

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -5,9 +5,8 @@ def safe_print_list(my_list=[], x=0):
     for i in range(0, x):
         try:
             print("{:d}".format(my_list[i], end=""))
-            start = start + 1
-
-        except:
+            start = 1 + start
+        except IndexError:
             break
 
     print()
