ee3ffe38725172a0502ed74d60d698e0b67352e1
avocadoclouds <rehana447@gmail.com>
2021-12-07 06:39:13 -0800

    add

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""respresents a class Square."""
+
+
+class Square:
+    """Represents a square.
+    Size: Private object attribute.
+    Size attribute: size of a side of the square.
+    No value/type instantiation."""
+    def __init__(self, size):
+        """Initializes the data."""
+        self.__size = size
