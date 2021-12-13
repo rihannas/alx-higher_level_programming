2f51b6448be8e198fd6a4622c8ca7a0402c98338
avocadoclouds <rehana447@gmail.com>
2021-12-13 15:08:59 -0800

    add

--- 0x08-python-more_classes/7-rectangle.py
+++ 0x08-python-more_classes/7-rectangle.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""Module 1-rectangle
+"""Module 7-rectangle
 Defines a Rectangle class.
 """
 
@@ -8,10 +8,10 @@ class Rectangle:
     """Rectangle class defined by width and height.
 
     Attributes:
-        Public class attribute number_of_instances:
-        Initialized to 0
-        Incremented during each new instance instantiation
-        Decremented during each instance deletion"""
+        number_of_instances: number of Rectangle instances,
+        increments with every instantitation,
+        decrements with every deletion
+    """
 
     number_of_instances = 0
     print_symbol = '#'
@@ -26,6 +26,29 @@ class Rectangle:
         self.width = width
         self.height = height
         Rectangle.number_of_instances += 1
+
+    def __str__(self):
+        """Returns an informal and nicely printable string representation
+        of a Rectangle instance, filled with the '#' character."""
+        if self.__height == 0 or self.__width == 0:
+            return ''
+        rec_str = ''
+        for i in range(self.__height):
+            for j in range(self.__width):
+                rec_str += str(self.print_symbol)
+            rec_str += '\n'
+        return rec_str[:-1]
+
+    def __repr__(self):
+        """Return a string representation of a Rectangle instance
+        that is able to recreate a new instance by using eval()
+        """
+        return "Rectangle({}, {})".format(self.__width, self.__height)
+
+    def __del__(self):
+        """Deletes a Rectangle instance."""
+        print("Bye rectangle...")
+        Rectangle.number_of_instances -= 1
 
     @property
     def width(self):
@@ -64,51 +87,19 @@ class Rectangle:
         self.__height = value
 
     def area(self):
-        """Calculates the area of rectangle
+        """Calculates the area of a Rectangle instance
 
-        Returns: Area of rectange"""
-
-        area = self.__width * self.__height
-
-        return area
+        Returns:
+            Area of the the rectangle, given by height * width
+        """
+        return self.__width * self.__height
 
     def perimeter(self):
-        """Calculates the perimeter of rectangle
+        """Calculates the perimeter of a Rectangle instance
 
-        Returns: Perimeter of rectangle"""
-
-        if self.__width == 0 or self.__height == 0:
+        Returns:
+            Perimeter of the rectangle, given by 2 * (height + width)
+        """
+        if self.__height == 0 or self.__width == 0:
             return 0
-
-        perimeter = (2 * self.__width) + (2 * self.__height)
-
-        return perimeter
-
-    def __str__(self):
-        """
-        Prints the rectangle represented by the combinations of
-        the `#` character.
-        """
-        result = ""
-
-        if self.__height is 0 or self.__width is 0:
-            return result
-
-        for i in range(0, self.__height):
-            result += "{:s}".format(self.__width * "#")
-
-            if i + 1 is not self.__height:
-                result += "\n"
-
-        return result
-
-    def __repr__(self):
-        """Return a string representation of a Rectangle instance
-        that is able to recreate a new instance by using eval()
-        """
-        return "Rectangle({}, {})".format(self.__width, self.__height)
-
-    def __del__(self):
-        """Deletes a Rectangle."""
-        print("Bye rectangle...")
-        Rectangle.number_of_instances -= 1
+        return 2 * (self.__width + self.__height)
