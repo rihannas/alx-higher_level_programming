b577dd38a4042833720aac8991a78ebca071b5c3
avocadoclouds <rehana447@gmail.com>
2021-12-13 15:42:41 -0800

    add

--- 0x08-python-more_classes/7-rectangle.py
+++ 0x08-python-more_classes/7-rectangle.py
@@ -83,21 +83,25 @@ class Rectangle:
         Prints the rectangle represented by the combinations of
         the `#` character.
         """
-         if self.__width == 0 or self.__height == 0:
-            return ""
-        string_r = ''
+        result = ""
+
+        if self.__height is 0 or self.__width is 0:
+            return result
+
         for i in range(self.__height):
             for j in range(self.__width):
-                string_r += str(self.print_symbol)
+                result = result + str(self.print_symbol)
+
             if i < (self.height - 1):
-                string_r += '\n'
-        return string_r
+                result += '\n'
+
+        return result
 
     def __repr__(self):
         """Return a string representation of a Rectangle instance
         that is able to recreate a new instance by using eval()
         """
-        return "Rectangle({}, {})".format(self.__width, self.__height)
+        return "Rectangle({:d}, {:d})".format(self.__width, self.__height)
 
     def __del__(self):
         """Deletes a Rectangle."""
