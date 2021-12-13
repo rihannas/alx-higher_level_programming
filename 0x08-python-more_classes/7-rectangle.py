160aae56c167113ee9dba81b273c9af4a14c4167
avocadoclouds <rehana447@gmail.com>
2021-12-13 15:21:20 -0800

    add

--- 0x08-python-more_classes/7-rectangle.py
+++ 0x08-python-more_classes/7-rectangle.py
@@ -51,8 +51,8 @@ class Rectangle:
 
     def __del__(self):
         """Deletes a Rectangle."""
-        Rectangle.number_of_instances -= 1
         print("Bye rectangle...")
+        Rectangle.number_of_instances -= 1
 
     @property
     def width(self):
