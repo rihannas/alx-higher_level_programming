8c8b520cd28a9ff30dd93c8e5c98f481df6e3dbc
avocadoclouds <rehana447@gmail.com>
2021-12-21 11:16:01 -0800

    add

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -1,15 +1,9 @@
 #!/usr/bin/python3
-"""
-the `number_of_lines` function
-"""
+"Method Module"
 
 
-def number_of_lines(filename=""):
-    """
-    Returns the number of lines of a text file
-
-    Args:
-        filename: thg name of the file
-    """
-    with open(filename, mode="r") as a_file:
-        return len(a_file.readlines())
+def write_file(filename="", text=""):
+    """function that writes a string to a text file (UTF8)
+    and returns the number of characters written"""
+    with open(filename, 'w', encoding="utf-8") as f:
+        return(f.write(text))
