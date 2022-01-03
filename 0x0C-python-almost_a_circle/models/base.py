1efe52dba18793f209900c8b5cf3c0bbc81f9a52
avocadoclouds <rehana447@gmail.com>
2022-01-02 23:27:38 -0800

    add

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,11 +1,23 @@
+#!/usr/bin/python3
+"""Module base.
+Defines a Base class for other classes in the project.
+"""
+
+
 class Base:
-  __nb_objects = 0
+    """Class with:
+    Private class attribute: __nb_objects
+    """
+    __nb_objects = 0
 
-  def __init__(self, id=None):
+    def __init__(self, id=None):
+        """Initialization of a Base instance.
+        Args:
+            - id: id of the instance
+        """
+        if id is not None:
+            self.id = id
 
-    if id is not None:
-      self.id = id
-
-    elif id == None:
-      Base.__nb_objects = Base.__nb_objects +1
-      self.id = Base.__nb_objects
+        elif id is None:
+            Base.__nb_objects = Base.__nb_objects + 1
+            self.id = Base.__nb_objects
