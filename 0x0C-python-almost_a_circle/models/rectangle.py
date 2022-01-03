308c3cad4f84558976f423e1eca256bee653e957
avocadoclouds <rehana447@gmail.com>
2022-01-03 01:11:47 -0800

    add

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -7,56 +7,52 @@ from models.base import Base
 
 
 class Rectangle(Base):
-    """Class describing a rectangle.
-    Priavte instance:
-        - width
-        - height
-        - x
-        - y
+    """ The `Rectangle` class
     """
-
     def __init__(self, width, height, x=0, y=0, id=None):
-        super().__init__(id=None)
 
+        """ The constructor of the `Rectangle` class
+        Attrs:
+            width: a width of a rectangle
+            height: a height of a rectangle
+            x: the x coordinate
+            y: the y coordinate
+            id: optional id
+        """
+        super().__init__(id)
         self.__width = width
+        self.__height = height
+        self.__x = x
+        self.__y = y
 
-        #getter for width
+        """getter for width"""
         def get_width(self):
             return self.__width
 
-        #setter for width
+        """setter for width"""
         def set_width(self, width):
             self.__width = width
 
-
-        self.__height = height
-
-        #getter for height
+        """getter for height"""
         def get_height(self):
             return self.__height
 
-        #setter for height
+        """setter for height"""
         def set_height(self, height):
             self.__height = height
 
-        self.__x = x
-
-        #getter for x
+        """getter for x"""
         def get_x(self):
             return self.__x
 
-        #setter for x
+        """setter for x"""
         def set_x(self, x):
-        self.__x = x
+            self.__x = x
 
-        self.__y = y
-
-        #getter for y
+        """getter for y"""
         def get_y(self):
             return self.__y
 
-        #setter for y
+        """setter for y"""
         def set_y(self, y):
             self.__y = y
-
-        super().__init__(id)
