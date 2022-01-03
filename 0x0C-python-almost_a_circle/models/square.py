d041a2ea119280cbc4830bf1e975a8deabafa43d
avocadoclouds <rehana447@gmail.com>
2022-01-03 03:43:20 -0800

    add

---
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+""" Module contains the `Square` class
+"""
+from .rectangle import Rectangle
+
+
+class Square(Rectangle):
+    """ The `Square` class
+    """
+
+    def __init__(self, size, x=0, y=0, id=None):
+         """ The constructor of the `Square` class
+        Attrs:
+            size: a size of a squareç
+            x: a top padding
+            y: a bottom padding
+            id: optional id
+        """
+
+        super().__init__(size, size, x, y, id)
+        self.size = size
+
+        def __str__(self):
+        """ Prints the instance in human readable format
+        """
+        return "[Square] ({}) {}/{} - {}".format(self.id,
+                                                 self.x, self.y,
+                                                 self.width)
