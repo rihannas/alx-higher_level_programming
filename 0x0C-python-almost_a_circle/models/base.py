57ec9fd15d51e2b38b013c5e58ed478df05ffa62
avocadoclouds <rehana447@gmail.com>
2022-01-03 04:33:12 -0800

    add

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,32 +1,59 @@
 #!/usr/bin/python3
-"""Module base.
-Defines a Base class for other classes in the project.
+""" Module contains the `Base` class
 """
-
 import json
 
 
 class Base:
-    """Class with:
-    Private class attribute: __nb_objects
+    """ The `Base` class
     """
     __nb_objects = 0
 
     def __init__(self, id=None):
-        """Initialization of a Base instance.
-        Args:
-            - id: id of the instance
-        """
-        if id is not None:
-            self.id = id
+        """ Constructor of the `Base` class
 
-        elif id is None:
-            Base.__nb_objects = Base.__nb_objects + 1
-            self.id = Base.__nb_objects
+        Args:
+            id: the uniqe id
+        """
+        if id is None:
+            Base.__nb_objects += 1
+            id = self.__nb_objects
+
+        self.id = id
+
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        """ Returns the JSON string representation of the instance
+
+        Args:
+            list_dictionaries: the list of dictionaries
+        """
+        if not list_dictionaries or len(list_dictionaries) is 0:
+            return "[]"
+
+        return json.dumps(list_dictionaries)
+
+    @classmethod
+    def save_to_file(cls, list_objs):
+        """ Writes the JSON string representation of list_objs to a file
+
+        Args:
+            cls: the reference to the class
+            list_objs: the list of objects to save
+        """
+        content = []
+
+        if list_objs is not None:
+            for i in list_objs:
+                content.append(cls.to_dictionary(i))
+
+        with open(cls.__name__ + ".json", "w", encoding="utf-8") as a_file:
+            a_file.write(cls.to_json_string(content))
 
     @staticmethod
     def from_json_string(json_string):
         """ Returns the list of the JSON string representation
+
         Args:
             json_string: string representing a list of dictionaries
         """
@@ -38,6 +65,7 @@ class Base:
     @classmethod
     def create(cls, **dictionary):
         """ returns an instance with all attributes already set
+
         Args:
             cls: the reference to the class
             dictionary: the double pointer to a dictionary
@@ -54,6 +82,7 @@ class Base:
     @classmethod
     def load_from_file(cls):
         """ Returns a list of instances
+
         Args:
             cls: the reference to the class
         """
