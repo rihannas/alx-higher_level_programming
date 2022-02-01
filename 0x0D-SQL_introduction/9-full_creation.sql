3a8e6386c436713383dec495024c2112bff4fce5
avocadoclouds <rehana447@gmail.com>
2022-02-01 02:39:09 -0800

    second table

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -3,7 +3,7 @@
 -- id INT
 -- name VARCHAR(256)
 -- score INT
-CREATE TABLES IF NOT EXISTS second_tables(id INT, name VARCHAR(256), SCORE INT);-- ADDING ROWS
+CREATE TABLE IF NOT EXISTS second_table (id INT, name VARCHAR(256), SCORE INT);-- ADDING ROWS
 INSERT INTO `second_table` (`id`, `name`, `score`) VALUES (1, "John", 10);
 INSERT INTO `second_table` (`id`, `name`, `score`) VALUES (2, "Alex", 3);
 INSERT INTO `second_table` (`id`, `name`, `score`) VALUES (3, "Bob", 14);
