c08caf73a37fc2f13d137fdc69220b86bfe10e93
avocadoclouds <rehana447@gmail.com>
2022-02-28 11:38:45 -0800

    add

---
+++ 0x12-javascript-warm_up/8-square.js
@@ -0,0 +1,22 @@
+#!/usr/bin/node
+if (isNaN(process.argv[2])) {
+  console.log('Missing number of occurrences');
+}
+
+if (process.argv[2] === undefined) {
+  console.log('Missing number of occurrences');
+} else {
+  let i;
+  let j;
+  const x = Number(process.argv[2]);
+  let sqr = '';
+
+  for (i = 0; i < x; i++) {
+    
+    for (j = 0; j < x; j++) {
+      sqr += 'X';
+    }
+    sqr += '\n';
+  }
+  console.log(sqr);      
+};
