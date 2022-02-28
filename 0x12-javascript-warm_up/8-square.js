08a954eb7711e97e4fb5d6ca0080d06252c366ec
avocadoclouds <rehana447@gmail.com>
2022-02-28 12:58:09 -0800

    while loop is easier

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -1,21 +1,14 @@
 #!/usr/bin/node
 if (isNaN(process.argv[2])) {
-  console.log('Missing number of occurrences');
-}
-
-if (process.argv[2] === undefined) {
-  console.log('Missing number of occurrences');
+  console.log('Missing size');
+} else if (process.argv[2] === undefined) {
+  console.log('Missing size');
 } else {
   let i;
-  let j;
   const x = Number(process.argv[2]);
-  let sqr = '';
 
-  for (i = 0; i < x; i++) {
-    for (j = 0; j < x; j++) {
-      sqr += 'X';
-    }
-    sqr += '\n';
+  while (i < x) {
+    console.log('X'.repeat(x));
+    i++;
   }
-  console.log(sqr);
 }
