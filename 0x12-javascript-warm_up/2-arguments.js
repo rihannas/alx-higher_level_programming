095362b58def1dde73879c14866f0670ec93a110
avocadoclouds <rehana447@gmail.com>
2022-02-28 12:23:49 -0800

    add

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,6 +1,10 @@
 #!/usr/bin/node
 if (process.argv[2] === undefined) {
   console.log('No argument');
-} else {
+} 
+if (process.argv[2) {
   console.log('Argument found');
+}
+else {
+  console.log('Arguments found');
 }
