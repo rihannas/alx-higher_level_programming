160bd27e1fda65dc1942db9279bd5f11835b7439
avocadoclouds <rehana447@gmail.com>
2022-02-28 11:26:11 -0800

    add

---
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+if (isNaN(process.argv[2])) {
+  console.log('Missing number of occurrences');
+}
+
+if (process.argv[2] === undefined) {
+  console.log('Missing number of occurrences');
+} else {
+  let i = 0;
+  const x = Number(process.argv[2]);
+  for (i; i < x; i++) {
+    console.log('C is fun');
+  }
+}
