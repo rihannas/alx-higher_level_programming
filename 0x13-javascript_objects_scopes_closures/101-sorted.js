11076bed2856ac63a39162b23ecf426fea83896f
avocadoclouds <rehana447@gmail.com>
2022-03-01 14:26:31 -0800

    fix advanced

--- 0x13-javascript_objects_scopes_closures/101-sorted.js
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -1,14 +1,11 @@
 #!/usr/bin/node
-
-const dict = require('./101-data').dict;
-const result = {};
-
-for (let i in dict) {
-  if (result[dict[i]] === undefined) {
-    result[dict[i]] = [];
+const dict = require('./101-data.js').dict;
+let newDict = {};
+for (let key in dict) {
+  if (newDict[dict[key]] === undefined) {
+    newDict[dict[key]] = [key];
+  } else {
+    newDict[dict[key]].push(key);
   }
-
-  result[dict[i]].push(i);
 }
-
-console.log(result);
+console.log(newDict);
