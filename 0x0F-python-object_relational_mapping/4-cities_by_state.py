68d2f3b371e3635b30007621e15823c2010d8e59
avocadoclouds <rehana447@gmail.com>
2022-03-06 03:55:07 -0800

    switch importing if __name__...

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 """a script that lists all cities from the database hbtn_0e_4_usa"""
 
-if __name__ == '__main__':
+import MYSQLdb
+import sys
 
-    import MYSQLdb
-    import sys
+if __name__ == '__main__':
 
     db = MySQLdb.connect(host='localhost', port=3306,
                          user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
