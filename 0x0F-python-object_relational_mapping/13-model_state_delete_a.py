241cdd4ea235bc71b0e24995205c7ebaf63a9f04
avocadoclouds <rehana447@gmail.com>
2022-03-06 09:58:13 -0800

    add

--- 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
+++ 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
@@ -16,7 +16,7 @@ if __name__ == "__main__":
                                    sys.argv[3]), pool_pre_ping=True)
     Base.metadata.create_all(engine)
 
-    session = Session(engine)
+    session = Session()
     for state in session.query(State).filter(State.name.like('%a%')):
         session.delete(state)
     session.commit()
