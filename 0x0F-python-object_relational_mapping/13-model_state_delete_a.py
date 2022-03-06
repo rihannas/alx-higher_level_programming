3506a7a300dae3949705af576180fcc522e08371
avocadoclouds <rehana447@gmail.com>
2022-03-06 10:02:41 -0800

    add

--- 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
+++ 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
@@ -2,27 +2,28 @@
 """deletes all State objects with a name containing
 the letter a from the database hbtn_0e_6_usa"""
 
-
-import sys
-from model_state import Base, State
-from sqlalchemy import create_engine
-from sqlalchemy.orm import Session
-from sqlalchemy.schema import Table
-
-
 if __name__ == "__main__":
+    from sys import argv
 
     if len(argv) < 4:
         print("Error: this script requires 3 arguments")
         exit()
 
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
-                           .format(sys.argv[1], sys.argv[2],
-                                   sys.argv[3]), pool_pre_ping=True)
-    Base.metadata.create_all(engine)
+    from model_state import Base, State
+    from sqlalchemy import create_engine
+    from sqlalchemy.orm import sessionmaker
 
-    session = Session(engine)
-    for state in session.query(State).filter(State.name.like('%a%')):
-        session.delete(state)
+    engine = create_engine(
+        "mysql+mysqldb://{}:{}@localhost/{}"
+        .format(argv[1], argv[2], argv[3]), pool_pre_ping=True)
+
+    Base.metadata.create_all(engine)
+    Session = sessionmaker(bind=engine)
+
+    session = Session()
+    session.query(State) \
+        .filter(State.name.like('%a%')) \
+        .delete(synchronize_session=False)
+
     session.commit()
     session.close()
