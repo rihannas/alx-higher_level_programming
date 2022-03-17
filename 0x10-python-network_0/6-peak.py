3e2dd9d1c0971e310d93d1c11b5feb7367893695
avocadoclouds <rehana447@gmail.com>
2022-03-18 02:03:31 +0300

    update to fix errs

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -8,13 +8,12 @@ def find_peak(list_of_integers):
     if list_of_integers == []:
         return None
 
-    size = len(list_of_integers)
-    if size == 1:
+    if len(list_of_integers) == 1:
         return list_of_integers[0]
-    elif size == 2:
+    elif len(list_of_integers) == 2:
         return max(list_of_integers)
 
-    mid = int(size / 2)
+    mid = len(list_of_integers) // 2
     peak = list_of_integers[mid]
     if peak > list_of_integers[mid - 1] and peak > list_of_integers[mid + 1]:
         return peak
