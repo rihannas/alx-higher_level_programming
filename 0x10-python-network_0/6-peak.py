c6aa8a85d31b357d7feb1abe3d7b49bbc0038215
avocadoclouds <rehana447@gmail.com>
2022-03-18 01:49:32 +0300

    update

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -4,17 +4,14 @@
 def find_peak(list_of_integers):
     """finds a peak in list_of_integers"""
 
-    if list_of_integers is None:
-        return None
-
-    elif list_of_integers == []:
+    if list_of_integers is None or list_of_integers == []:
         return None
 
     if len(list_of_integers) == 1:
         return list_of_integers[0]
     elif len(list_of_integers) == 2:
         return max(list_of_integers)
-        
+
     mid = len(list_of_integers) // 2
 
     if len(list_of_integers) != mid + 1:
