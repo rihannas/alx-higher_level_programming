cad6e470e38dfc1e7923f314fd1aac052fd45fff
avocadoclouds <rehana447@gmail.com>
2022-03-19 00:51:37 +0300

    fix syntax err

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -6,7 +6,7 @@ import requests
 import sys
 
 
-if __name__ = "__main__":
+if __name__ == "__main__":
 
     r = requests.get('https://api.github.com/user',
                          auth=(sys.argv[1], sys.argv[2]))
