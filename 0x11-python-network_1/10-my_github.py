f07c5f108c892e2612c26df3da922423803b3fc0
avocadoclouds <rehana447@gmail.com>
2022-03-19 00:49:53 +0300

    fix syntax err

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -10,7 +10,5 @@ if __name__ = "__main__":
 
     r = requests.get('https://api.github.com/user',
                          auth=(sys.argv[1], sys.argv[2]))
-    if r.status_code >= 400:
-        print('None')
-    else:
-        print(r.json().get('id'))
+    result = response.json()
+    print(result.get("id"))
