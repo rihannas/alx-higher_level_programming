a9cdcf4200ac54fe9e4d32f0a19ba9f1bbc116c2
avocadoclouds <rehana447@gmail.com>
2022-03-19 00:49:01 +0300

    fix syntax err

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -1,15 +1,15 @@
 #!/usr/bin/python3
 """a Python script that takes your
 GitHub credentials (username and password)
-and uses the GitHub API to display your id"""
+and uses the GitHub API to display your id."""
 import requests
 import sys
 
 
-if __name__ = "__main__"
+if __name__ = "__main__":
 
-r = requests.get('https://api.github.com/user',
-                     auth=(sys.argv[1], sys.argv[2]))
+    r = requests.get('https://api.github.com/user',
+                         auth=(sys.argv[1], sys.argv[2]))
     if r.status_code >= 400:
         print('None')
     else:
