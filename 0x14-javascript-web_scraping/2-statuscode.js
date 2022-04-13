37a655ed820f1f1a26e7eb0648b9a5acdcbe36b3
avocadoclouds <rehana447@gmail.com>
2022-04-12 19:12:49 -0700

    fix typo

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -4,9 +4,9 @@
 const request = require('request');
 const URL = process.argv[2];
 
-resquest(URL, function (error, response) {
+request(URL, function (error, response) {
   if (error) {
     console.error(error);
   }
-  console.log('code: ', response && response.statusCode);
+  console.log('code: ' + response.statusCode);
 });
