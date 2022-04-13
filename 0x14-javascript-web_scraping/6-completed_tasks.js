1d88b151737afb1094864856a07b943de3663042
avocadoclouds <rehana447@gmail.com>
2022-04-12 19:16:47 -0700

    fix syntax error

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -2,7 +2,7 @@
 // a scriipt that computes the number of tasks completed by user id.
 
 const request = require('request');
-const API_URL: 'https://jsonplaceholder.typicode.com/todos';
+const API_URL = 'https://jsonplaceholder.typicode.com/todos';
 
 request(API_URL, function (error, response, body) {
   if (error) {
