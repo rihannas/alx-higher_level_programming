46a14446b3def65aa13a6c789cabf30c37e98912
avocadoclouds <rehana447@gmail.com>
2022-04-21 04:32:09 +0300

    remove

--- 8-script.js
+++
@@ -1,5 +0,0 @@
-$.get('https://swapi.co/api/films/?format=json', function (data) {
-  $.each(data.results, function (index, value) {
-    $('<li>' + value.title + '</li>').appendTo('UL#list_movies');
-  });
-});
