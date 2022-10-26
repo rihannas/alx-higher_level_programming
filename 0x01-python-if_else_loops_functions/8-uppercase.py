c0057033b986cb195de6cbe222087f56d5196dfc
Rihanna <75071112+avocadoclouds@users.noreply.github.com>
2022-10-26 17:51:57 +0300

    fix err

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -5,5 +5,10 @@ def uppercase(str):
         if ord(char) >= 97 and ord(char) <= 123:
             char = ord(char) - 32
             char = chr(char)
-            print('{}'.format(char), end="")
+        else:
+            pass
+        print('{}'.format(char), end="")
     print()
+
+
+uppercase('')
