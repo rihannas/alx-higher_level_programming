c31c67cb68232d8b67c2cd5dcfa6b55c2b621a65
Rihanna <75071112+avocadoclouds@users.noreply.github.com>
2022-10-26 17:34:44 +0300

    fix err

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,9 +1,13 @@
 #!/usr/bin/python3
+"""
+a function that prints a string in uppercase followed by a new line.
+"""
+
 
 def uppercase(str):
     for char in str:
-        if ord(char) not in range(65, 91):
-            char = ord(char) + 32
+        if ord(char) in range(97, 123):
+            char = ord(char) - 32
             char = chr(char)
-            print('{}'.format(char))
+            print('{}'.format(char), end="")
     print()
