ebf6059997f9ce7a4ac93ca22f65e5c2ddbbebf2
Rihanna <75071112+avocadoclouds@users.noreply.github.com>
2022-10-27 14:38:31 +0300

    update

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python3
 def element_at(my_list, idx):
-    element = len(my_list) - 1
     if idx < 0:
         return None
-    elif idx > element:
+    elif idx > len(my_list):
         return None
     else:
         return my_list[idx]
