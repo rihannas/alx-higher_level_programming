08b846af109bfbcbe0fa740ae2d54975db434bb6
Rihanna <75071112+avocadoclouds@users.noreply.github.com>
2022-10-28 20:52:45 +0300

    update

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,7 +1,17 @@
 #!/usr/bin/python3
+
 def no_c(my_string):
-    new_string = ''
-    for i in (my_string):
-        if i != 'c' and i != 'C':
-            new_string += i
-    return new_string
+    if "c" in my_string:
+        noC = my_string.replace("c", "")
+
+    if "C" in my_string:
+        noC = my_string.replace("C", "")
+    return noC
+
+
+# def no_c(my_string):
+#     new_string = ''
+#     for i in (my_string):
+#         if i != 'c' and i != 'C':
+#             new_string += i
+#     return new_string
