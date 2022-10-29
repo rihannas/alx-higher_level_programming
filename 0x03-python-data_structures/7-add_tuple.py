e2f2d9ec236d8abcd9d657cc3ff0eb84edef910d
Rihanna <75071112+avocadoclouds@users.noreply.github.com>
2022-10-29 16:26:54 +0300

    task 7

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -1,10 +1,27 @@
 #!/usr/bin/python3
 def add_tuple(tuple_a=(), tuple_b=()):
-    la = len(tuple_a)
-    lb = len(tuple_b)
 
-    if la < 2 or lb < 2:
-        tuple_a = tuple_a + (0, 0)
-        tuple_b = tuple_b + (0, 0)
-    new = tuple_a[0] + tuple_b[0], tuple_a[1] + tuple_b[1]
-    return new
+    # new
+    if len(tuple_a) < 2:
+        if len(tuple_a) == 1:
+            tuple_a = tuple_a + (0, )
+        elif len(tuple_a) == 0:
+            tuple_a = (0, 0)
+    if len(tuple_b) < 2:
+        if len(tuple_b) == 1:
+            tuple_b = tuple_b + (0, )
+        elif len(tuple_b) == 0:
+            tuple_b = (0, 0)
+
+    new_tuple = (tuple_a[0] + tuple_b[0], tuple_a[1] + tuple_b[1])
+    return new_tuple
+
+    # old
+    # la = len(tuple_a)
+    # lb = len(tuple_b)
+
+    # if la < 2 or lb < 2:
+    #     tuple_a = tuple_a + (0, 0)
+    #     tuple_b = tuple_b + (0, 0)
+    # new = tuple_a[0] + tuple_b[0], tuple_a[1] + tuple_b[1]
+    # return new
