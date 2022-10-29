4ab448ebe0363513bc0429a8d6b249dd8c7b1213
Rihanna <75071112+avocadoclouds@users.noreply.github.com>
2022-10-29 15:48:10 +0300

    task 6

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,9 +1,7 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
-    for i in matrix:
-        for j in i:
-            if j != i[-1]:
-                print("{:d}".format(j), end=" ")
-            else:
-                print("{:d}".format(j), end="")
+
+    for list in matrix:
+        for n in list:
+            print("{} ".format(n), end="")
         print()
