646d08b30e77f602d6e89bb99e42be0352b80101
Rihanna <75071112+avocadoclouds@users.noreply.github.com>
2022-10-29 23:02:24 +0300

    task 9

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -1,10 +1,18 @@
 #!/usr/bin/python3
 def max_integer(my_list=[]):
+    # new
     if len(my_list) == 0:
         return None
     else:
-        maxx = my_list[0]
-        for i in my_list:
-            if i > maxx:
-                maxx = i
-        return maxx
+        my_list.sort()
+        return my_list[-1]
+
+    # old
+    # if len(my_list) == 0:
+    #     return None
+    # else:
+    #     maxx = my_list[0]
+    #     for i in my_list:
+    #         if i > maxx:
+    #             maxx = i
+    #     return maxx
