9aab1f775edfe260c89d134a76938a38029bfbcf
Rihanna <75071112+avocadoclouds@users.noreply.github.com>
2022-10-29 23:35:28 +0300

    task 11

--- 0x03-python-data_structures/11-delete_at.py
+++ 0x03-python-data_structures/11-delete_at.py
@@ -1,10 +1,20 @@
 #!/usr/bin/python3
 def delete_at(my_list=[], idx=0):
-    element = len(my_list)
-    if idx < 0:
-        return my_list
-    elif idx > (element - 1):
-        return my_list
-    else:
-        del my_list[idx]
+
+    # new
+    new = my_list.copy()
+    my_list.clear()
+    for num in new:
+        if num != new[idx]:
+            my_list.append(num)
     return my_list
+
+    # old
+    # element = len(my_list)
+    # if idx < 0:
+    #     return my_list
+    # elif idx > (element - 1):
+    #     return my_list
+    # else:
+    #     del my_list[idx]
+    # return my_list
