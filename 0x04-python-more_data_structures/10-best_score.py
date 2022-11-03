1db3a7d598357388eba52d9902f090c040e08645
Rihanna <75071112+avocadoclouds@users.noreply.github.com>
2022-11-04 01:12:35 +0300

    task 10

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,6 +1,18 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
-    if not a_dictionary:
+
+    if a_dictionary is None:
         return None
-    maxx = max(a_dictionary)
-    return maxx
+
+    max = 0
+    values = a_dictionary.values()
+    for value in values:
+
+        if value > max:
+            max = value
+    # key = [k for k, v in a_dictionary.items() if v == max]
+
+    # returning a key froma value
+    for key, value in a_dictionary.items():
+        if value == max:
+            return key
