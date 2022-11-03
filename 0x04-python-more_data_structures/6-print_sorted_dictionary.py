11d6c4ef49593f5fb228367a120303fd038e99f2
Rihanna <75071112+avocadoclouds@users.noreply.github.com>
2022-11-03 17:19:54 +0300

    task 6

--- 0x04-python-more_data_structures/6-print_sorted_dictionary.py
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -2,3 +2,11 @@
 def print_sorted_dictionary(a_dictionary):
     for i in sorted(a_dictionary):
         print("{}: {}".format(i, a_dictionary[i]))
+
+
+# def print_sorted_dictionary(a_dictionary):
+#     keys = a_dictionary.keys()
+#     keys = list(keys)
+#     keys.sort()
+#     for key in keys:
+#         print("{}: {}".format(key, a_dictionary[key]))
