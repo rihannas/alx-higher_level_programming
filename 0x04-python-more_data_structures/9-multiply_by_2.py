594009fd42a14a90053eca7d7b9cc0d553341c68
Rihanna <75071112+avocadoclouds@users.noreply.github.com>
2022-11-04 00:28:19 +0300

    task 9

--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -1,6 +1,15 @@
 #!/usr/bin/python3
 def multiply_by_2(a_dictionary):
-    new = {}
-    for i in a_dictionary:
-        new[i] = a_dictionary[i] * 2
-    return new
+
+    # new
+    dic = a_dictionary.copy()
+    for key, value in dic.items():
+        value = value * 2
+        dic[key] = value
+    return dic
+
+    # old
+    # new = {}
+    # for i in a_dictionary:
+    #     new[i] = a_dictionary[i] * 2
+    # return new
