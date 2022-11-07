62bf7d844dd0fa411aabea908d26dce8c14f74e6
Rihanna <75071112+avocadoclouds@users.noreply.github.com>
2022-11-07 03:55:31 +0300

    task 3

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -1,12 +1,26 @@
 #!/usr/bin/python3
 
 def safe_print_division(a, b):
+
+    # new
     try:
-        result = a / b
-        return result
-    except (ZeroDivisionError):
-        result = None
-        return result
+        c = a/b
+    except ZeroDivisionError:
+        c = None
     finally:
-        print("Inside result: {}".format(result))
-    return result
+        print("Inside result: {}".format(c))
+    if c is None:
+        return c
+
+    return c  # this will return if c is not None
+
+    # older code is better writing wise
+
+    # try:
+    #     result = a / b
+    #     return result
+    # except (ZeroDivisionError):
+    #     result = None
+    #     return result
+    # finally:
+    #     print("Inside result: {}".format(result))
