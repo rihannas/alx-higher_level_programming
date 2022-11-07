99d01470ee120bb8919e8f534a12d6131ab8ea6f
Rihanna <75071112+avocadoclouds@users.noreply.github.com>
2022-11-07 22:00:46 +0300

    task 4

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -7,7 +7,7 @@ def list_division(my_list_1, my_list_2, list_length):
         try:
             c = my_list_1[i]/my_list_2[i]
         except ZeroDivisionError:
-            print("division by zero")
+            print("division by 0")
             c = 0
         except TypeError:
             print("wrong type")
