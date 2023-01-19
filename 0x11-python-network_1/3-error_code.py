374417a4ccd2f280d697335b0c674f0f0fb681ec
Rihanna <75071112+avocadoclouds@users.noreply.github.com>
2023-01-19 19:20:21 +0300

    update

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -3,14 +3,13 @@
 sends a request to the URL and displays the body of the response"""
 
 if __name__ == "__main__":
-    import urllib.request
-    from urllib.error import HTTPError
-    import sys
+    from urllib import request, error
+    from sys import argv
 
-    body = urllib.request.Request(sys.argv[1])
-
+    req = request.Request(argv[1])
     try:
-        with urllib.request.urlopen(body) as res:
-            print(res.read().decode("utf-8"))
-    except HTTPError as err:
-        print("Error code: {}".format(err.code))
+        with request.urlopen(req) as r:
+            print(r.read().decode("utf-8"))
+
+    except error.HTTPError as e:
+        print(f'Error code: {e.code}')
