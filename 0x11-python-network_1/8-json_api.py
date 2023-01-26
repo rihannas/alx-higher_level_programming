d5202ba6d0c704f30a3a8fc3a546839641b77fcd
Rihanna <75071112+avocadoclouds@users.noreply.github.com>
2023-01-26 14:36:35 +0300

    task 8

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -4,23 +4,42 @@ and sends a POST request to http://0.0.0.0:5000/search_user
 with the letter as a parameter.
 """
 import requests
-import sys
+from sys import argv
 
 
 if __name__ == "__main__":
-    if len(sys.argv) > 1:
-        query = sys.argv[1]
-    else:
-        query = ""
-    try:
-        url = "http://0.0.0.0:5000/search_user"
-        data = {"q": query}
-        res = requests.post(url, data=data)
 
-        user = res.json()
-        if "name" in user and "id" in user:
-            print("[{}] {}".format(user["id"], user["name"]))
-        else:
-            print("No result")
-    except ValueError:
-        print("Not a valid JSON")
+    url = 'http://0.0.0.0:5000/search_user'
+
+    if argv[1] is not None:
+        value = argv[1]
+    else:
+        value = ''
+
+    res = requests.post(url, data={'q': value})
+    try:
+        json_res = res.json()
+    except:
+        print('Not a valid JSON')
+
+    if json_res is None:
+        print('No result')
+    else:
+        print(f'[{json_res.id}] {json_res.name}')
+
+    # if len(sys.argv) > 1:
+    #     query = sys.argv[1]
+    # else:
+    #     query = ""
+    # try:
+    #     url = "http://0.0.0.0:5000/search_user"
+    #     data = {"q": query}
+    #     res = requests.post(url, data=data)
+
+    #     user = res.json()
+    #     if "name" in user and "id" in user:
+    #         print("[{}] {}".format(user["id"], user["name"]))
+    #     else:
+    #         print("No result")
+    # except ValueError:
+    #     print("Not a valid JSON")
