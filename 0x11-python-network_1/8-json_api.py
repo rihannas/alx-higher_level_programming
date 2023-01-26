5863eb64b2dcc4b8e79bed5a190fc73177539555
Rihanna <75071112+avocadoclouds@users.noreply.github.com>
2023-01-26 14:47:16 +0300

    task 8

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -9,37 +9,19 @@ from sys import argv
 
 if __name__ == "__main__":
 
-    url = 'http://0.0.0.0:5000/search_user'
-
     if len(argv) > 1:
         query = argv[1]
     else:
         query = ""
-
-    res = requests.post(url, data={'q': query})
     try:
-        json_res = res.json()
+        url = "http://0.0.0.0:5000/search_user"
+        data = {"q": query}
+        res = requests.post(url, data=data)
+
+        user = res.json()
+        if "name" in user and "id" in user:
+            print("[{}] {}".format(user["id"], user["name"]))
+        else:
+            print("No result")
     except ValueError:
-        print('Not a valid JSON')
-
-    if json_res is None:
-        print('No result')
-    else:
-        print("[{}] {}".format(json_res["id"], json_res["name"]))
-
-    # if len(sys.argv) > 1:
-    #     query = sys.argv[1]
-    # else:
-    #     query = ""
-    # try:
-    #     url = "http://0.0.0.0:5000/search_user"
-    #     data = {"q": query}
-    #     res = requests.post(url, data=data)
-
-    #     user = res.json()
-    #     if "name" in user and "id" in user:
-    #         print("[{}] {}".format(user["id"], user["name"]))
-    #     else:
-    #         print("No result")
-    # except ValueError:
-    #     print("Not a valid JSON")
+        print("Not a valid JSON")
