a8a68afa85198c762b62c5fe918237e32cdb1274
Rihanna <75071112+avocadoclouds@users.noreply.github.com>
2023-01-26 11:15:51 +0300

    task 5

--- 0x11-python-network_1/5-hbtn_header.py
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -2,11 +2,20 @@
 """a Python script that takes in a URL, sends a request to the URL
 and displays the value of the variable X-Request-Id
  in the response header"""
+# import requests
+# import sys
+
+
+# if __name__ == "__main__":
+
+#     res = requests.get(sys.argv[1])
+#     print(res.headers.get("X-Request-Id"))
+
+# new
 import requests
-import sys
+from sys import argv
 
+url = argv[1]
 
-if __name__ == "__main__":
-
-    res = requests.get(sys.argv[1])
-    print(res.headers.get("X-Request-Id"))
+res = requests.get(url)
+print(res.headers['X-Request-Id'])
