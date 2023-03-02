5d2ba5df8350265d6dad29412c0c2ddabf6da8d5
Rihanna <75071112+avocadoclouds@users.noreply.github.com>
2023-03-02 11:36:14 +0300

    fix issue

--- 0x0F-python-object_relational_mapping/102-relationship_cities_states_list.py
+++ 0x0F-python-object_relational_mapping/102-relationship_cities_states_list.py
@@ -7,16 +7,18 @@ if __name__ == "__main__":
     import sys
     from model_state import Base, State
     from sqlalchemy import create_engine
-    from sqlalchemy.orm import Session
+    from sqlalchemy.orm import Session, sessionmaker
     from sqlalchemy.schema import Table
+    from relationship_state import State
+    from relationship_city import City
 
     engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
                            .format(sys.argv[1], sys.argv[2],
                                    sys.argv[3]), pool_pre_ping=True)
     Base.metadata.create_all(engine)
+    Session = sessionmaker(bind=engine)  # this creates the session
+    session = Session()
+    for city in session.query(City).order_by(City.id):
+        print("{}: {} -> {}".format(city.id, city.name, city.state.name))
 
-    session = Session(engine)
-    state = session.query(State).filter(State.id == 2).first()
-    state.name = 'New Mexico'
-    session.commit()
     session.close()
