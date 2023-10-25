86a7a8fc07ea86d80c1a717c1432a33dee09a996
Rihanna <75071112+avocadoclouds@users.noreply.github.com>
2023-10-25 12:47:42 +0300

    update code

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,8 +1,20 @@
 #!/usr/bin/node
-if (process.argv.length <= 3) {
-  console.log('0');
+
+let sliced = process.argv.slice(2)
+if (sliced.length < 2) {
+    console.log(0)
 } else {
-  const arr = process.argv.slice(2).map(Number);
-  const second = arr.sort(function (a, b) { return b - a; })[1];
-  console.log(second);
+    let largestNumber = Math.max(...sliced);
+    sliced.pop(largestNumber)
+    largestNumber = Math.max(...sliced);
+    console.log(largestNumber)
 }
+
+
+// if (process.argv.length <= 3) {
+//   console.log('0');
+// } else {
+//   const arr = process.argv.slice(2).map(Number);
+//   const second = arr.sort(function (a, b) { return b - a; })[1];
+//   console.log(second);
+// }
