9f7c173a88c9017d2313a2c9eb7877bb183005f8
Rihanna <75071112+avocadoclouds@users.noreply.github.com>
2023-10-25 11:55:58 +0300

    update code

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -1,14 +1,29 @@
 #!/usr/bin/node
-if (isNaN(process.argv[2])) {
-  console.log('Missing size');
-} else if (process.argv[2] === undefined) {
-  console.log('Missing size');
-} else {
-  let i = 0;
-  const x = Number(process.argv[2]);
 
-  while (i < x) {
-    console.log('X'.repeat(x));
-    i++;
-  }
+let size = process.argv[2]
+
+if (isNaN(size)) {
+    console.log('Missing size')
+} else {
+for (let i = 0; i < size; i++) {
+    row = ''
+    for (let j = 0; j < size; j++) {
+    row += 'X'
+    }
+    console.log(row)
 }
+}
+
+// if (isNaN(process.argv[2])) {
+//   console.log('Missing size');
+// } else if (process.argv[2] === undefined) {
+//   console.log('Missing size');
+// } else {
+//   let i = 0;
+//   const x = Number(process.argv[2]);
+
+//   while (i < x) {
+//     console.log('X'.repeat(x));
+//     i++;
+//   }
+// }
